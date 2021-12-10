b5aa0caaa39ee6a82699b6ab0c923b7c071b69e4
Artsiom Fedarkevich <artsiom.fedarkevich@innowise-group.com>
2021-12-10 17:21:12 +0300

    Something

--- testcase.py
+++ testcase.py
@@ -2,7 +2,7 @@ from forimport import get_number
 
 print("Print")
 def test():
-    print('HellOUIN_world')
+    print('HellOUIN_worfld')
     print("Something")
 if __name__ == '__main__':
     test()
